1a5cbe67130023926f10fe0fb29867d70485b3b0
mickyginger <mickyginger@gmail.com>
2015-12-21 19:04:01 +0000

    intial commit

---
+++ api/app.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var app = express();
+
+var PORT = process.env.PORT || 3000;
+var server = app.listen(PORT);
+var io = require('socket.io').listen(server);
+
+var channels = {};
+
+io.on('connection', function(socket) {
+  console.log(channels);
+  socket.emit('allChannels', channels);
+  
+  socket.on('message', function(data) {
+    console.log(data);
+    channels[data.channel].push(data.message);
+    io.to(data.channel).emit('message', data);
+  });
+  
+  socket.on('createChannel', function(channel) {
+    channels[channel] = [];
+    socket.join(channel);
+    socket.emit("newChannel", channel);
+  });
+
+  socket.on('joinChannel', function(channel) {
+    socket.join(channel);
+  });
+});
+
+console.log("Express is listening to port " + PORT);
---
+++ frontend/js/app.js
@@ -0,0 +1,54 @@
+angular
+  .module('angularChat', [])
+  .controller('ChatController', ['$scope', function($scope) {
+
+    var socket = io('http://localhost:3000');
+
+    $scope.message = "";
+    $scope.messages = [];
+
+    $scope.channel = "";
+    $scope.channels = {};
+
+    $scope.currentChannel = null;
+
+    $scope.chat = function() {
+      socket.emit("message", { channel: $scope.currentChannel, message: $scope.message });
+      $scope.message = "";
+    }
+
+    $scope.createChannel = function() {
+      if(!$scope.channels[$scope.channel]) {
+        socket.emit("createChannel", $scope.channel);
+        $scope.currentChannel = $scope.channel;
+        $scope.channel = "";
+      }
+    }
+
+    $scope.showChannel = function(channel) {
+      socket.emit('joinChannel', channel);
+      $scope.currentChannel = channel;
+      $scope.messages = $scope.channels[channel];
+    }
+
+    socket.on('allChannels', function(channels) {
+      $scope.$evalAsync(function() {
+        console.log($scope.channels);
+        $scope.channels = channels;
+      });
+    });
+
+    socket.on('message', function(data) {
+      console.log("MSG RECEIVED!", data);
+      $scope.$evalAsync(function() {
+        $scope.channels[data.channel].push(data.message);
+      });
+    });
+
+    socket.on('newChannel', function(name) {
+      $scope.$evalAsync(function() {
+        $scope.channels[name] = [];
+      });
+    });
+
+  }]);
